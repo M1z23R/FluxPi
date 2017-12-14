6781f166c5619677930e805557a530dc130d4902
M1z23R <dimitrijerandjelovic@hotmail.com>
2017-12-14 16:30:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
-sudo apt-get install isc-dhcp-server hostapd lighttpd pyrit mdk3 xterm aircrack-ng
+sudo apt-get update
+sudo apt-get install isc-dhcp-server hostapd lighttpd pyrit mdk3 xterm aircrack-ng libpcap-dev libssl-dev
