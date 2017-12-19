65104ef056bcee1075531c6ad782529b88b0a655
M1z23R <dimitrijerandjelovic@hotmail.com>
2017-12-19 23:30:52 +0000

    Update rpi_menu.sh

--- WorkInProgress - Dont use/rpi_menu.sh
+++ WorkInProgress - Dont use/rpi_menu.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 clear
-COLUMNS=$(tput cols) 
 readonly CRed="\e[0;31m"
 readonly CGrn="\e[0;32m"
 readonly CYel="\e[0;33m"
@@ -10,48 +9,97 @@ readonly CCyn="\e[0;36m"
 readonly CGry="\e[0;37m"
 readonly CWht="\e[0;37m"
 readonly CClr="\e[0m"
-echo -e "${CWht}F$CClr${CRed}lux$CClr${CWht}P$CClr${CRed}i$CClr"
-printf '\n'
+currentmenu=1;
 
 
-
-while [ "$opt" != "1" ] && [ "$opt" != "2" ] && [ "$opt" != "3" ] && [ "$opt" != "4" ] && [ "$opt" != "5" ] && [ "$opt" != "0" ];
+while [ "$currentmenu" != "0" ];
 do
-echo -e 'What would you like to do first ?'
-printf '\n'
-echo -e "${CRed}[$CClr${CWht}1$CClr${CRed}]$CClr ${CWht}Start the Captive Portal on$CClr ${CRed}Pi$CClr"
-echo -e "${CRed}[$CClr${CWht}2$CClr${CRed}]$CClr ${CWht}Capture Handshake on$CClr ${CRed}Pi$CClr"
-echo -e "${CRed}[$CClr${CWht}3$CClr${CRed}]$CClr ${CWht}Run dependency installer on$CClr ${CRed}Pi$CClr"
-echo -e "${CRed}[$CClr${CWht}4$CClr${CRed}]$CClr ${CWht}Create re4son-kernel installer on$CClr ${CRed}Pi$CClr"
-echo -e "${CRed}[$CClr${CWht}5$CClr${CRed}]$CClr ${CWht}Read the README file$CClr"
-printf '\n'
-echo -e "${CRed}[$CClr${CWht}0$CClr${CRed}]$CClr ${CRed}Exit$CClr"
-read -e -n 1 opt
-printf "\n"
-clear
-case $opt in
-	1) echo "Starting Captive Portal Attack"
-	;;
-	2) echo "Capturing Handshake"
-	;;
-	3) echo "Installing dependencies"
-	;;
-	4) echo "Installing re4son-kernel"
-	echo "\
-	sudo bash
-	mount /dev/mmcblk0p1 /boot
-	cd /usr/local/src
-	## For current stable
-	wget  -O re4son-kernel_current.tar.xz https://whitedome.com.au/re4son/downloads/11299/
-	tar -xJf re4son-kernel_current.tar.xz
-	cd re4son-kernel_4*
-	./install.sh" >> install-re4son.sh
-	chmod +x install-re4son.sh
-	;;
-	5) "README"
+
+case $currentmenu in
+	1)
 	clear
-	cat ./README
+	echo -e "${CWht}F$CClr${CRed}lux$CClr${CWht}P$CClr${CRed}i$CClr"
+	printf '\n'
+	echo -e 'What would you like to do first ?'
+	printf '\n'
+	echo -e "${CRed}[$CClr${CWht}1$CClr${CRed}]$CClr ${CWht}Start the Captive Portal on$CClr ${CRed}Pi$CClr"
+	echo -e "${CRed}[$CClr${CWht}2$CClr${CRed}]$CClr ${CWht}Capture Handshake on$CClr ${CRed}Pi$CClr"
+	echo -e "${CRed}[$CClr${CWht}3$CClr${CRed}]$CClr ${CWht}Run dependency installer on$CClr ${CRed}Pi$CClr"
+	echo -e "${CRed}[$CClr${CWht}4$CClr${CRed}]$CClr ${CWht}Create re4son-kernel installer on$CClr ${CRed}Pi$CClr"
+	echo -e "${CRed}[$CClr${CWht}5$CClr${CRed}]$CClr ${CWht}Read the README file$CClr"
+	printf '\n'
+	echo -e "${CRed}[$CClr${CWht}0$CClr${CRed}]$CClr ${CRed}Exit$CClr"
+	read -e -n 1 currentmenu
+	printf "\n"
+	;;
+	2)
+	clear
+	echo -e "Starting Captive Portal Attack"
+	currentmenu=2
+	;;
+	3)
+	clear
+	echo "Capturing Handshake"
+	currentmenu=3
+	;;
+	4) 
+	clear
+	echo "Installing dependencies"
+	currentmenu=4
+	;;
+	5)
+	clear
+	echo "Installing re4son-kernel"
+	currentmenu=5
+	;;
+	6)
+	clear
+	echo "README"
+	currentmenu=6
+	;;
+	0)
+	currentmenu=0
 	;;
 esac
+
+	FILE=1
+	while [ "$currentmenu" == "2" ];
+	do
+	clear
+	
+	if [ -f "./dump-01.cap" ]; then
+	echo -e "Continue"
+	read new
+	elif [ "$FILE" == "0" ]; then
+	currentmenu=1
+	break
+	
+	elif [ -f $FILE ]; then
+	echo -e "Continue"
+	read new
+	else
+	echo -e "${CRed}E$CClr""nter path of .cap file with ${CRed}valid$CClr handshake:"
+	printf "\nPath to file: "
+	read FILE
+	
+	fi
+	
+	done
+	
+	
+	while [ "$currentmenu" == "3" ];
+	do
+	
+	read -e -n 1 opt
+	case $opt in
+		0)
+		currentmenu=1
+		;;
+		1)
+		echo -e "some settings"
+		;;
+	esac
+	done
+	
 done
 printf "Exited"
